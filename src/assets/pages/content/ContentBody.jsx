4dac9d3704d2a9da19f8b8e1377184c5baa37e09
Ananias Caetano <ananiascaetano556@gmail.com>
2025-03-17 19:26:07 -0300

    feat: adding pages in router and menu

--- src/assets/pages/content/ContentBody.jsx
+++ src/assets/pages/content/ContentBody.jsx
@@ -3,6 +3,8 @@ import ConvertText from "../convertText/ConvertText";
 import GenerateKey from "../generateKey/GenerateKey";
 import GenerateCpf from "../generateCpf/GenerateCpf";
 import GenerateCnpj from "../generateCnpj/GenerateCnpj";
+import JsonFormatter from "../jsonFormatter/JsonFormatter";
+import XmlFormatter from "../xmlFormatter/XmlFormatter";
 
 function ContentBody() {
   return ( 
@@ -13,6 +15,8 @@ function ContentBody() {
         <Route path='/generate-key' element={<GenerateKey />}></Route>
         <Route path='/generate-cpf' element={<GenerateCpf />}></Route>
         <Route path='/generate-cnpj' element={<GenerateCnpj />}></Route>
+        <Route path='/json-formatter' element={<JsonFormatter />}></Route>
+        <Route path='/xml-formatter' element={<XmlFormatter />}></Route>
       </Routes>
     </>
   );
